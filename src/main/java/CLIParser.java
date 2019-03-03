8f72589f73403fb90a82b17ab6629f7290ad33d6
Unknown <marionbouteille83@gmail.com>
2019-03-03 20:05:46 +0100

    base

---
+++ src/main/java/CLIParser.java
@@ -0,0 +1,41 @@
+
+import org.apache.commons.cli.*;
+
+import java.util.Arrays;
+
+public class CLIParser {
+
+    public static void main(String[] args) throws ParseException {
+        Options options = new Options();
+        //-enc|-dec
+        options.addOption("enc",false,"Chiffrement");
+        options.addOption("dec", false, "Déchiffrement");
+        options.addOption("in", true, "Fichier d'entré");
+        options.addOption("out", false, "Fichier de sorti");
+
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("CLI options : ", options);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        if(cmd.hasOption("enc")){
+            System.out.println(cmd);
+        }
+
+        if(cmd.hasOption("dec")){
+            System.out.println(cmd);
+        }
+        if(cmd.hasOption("in")){
+            System.out.println(cmd.getOptionValue("in"));
+        }
+        if(cmd.hasOption("out")){
+            System.out.println(Arrays.toString(cmd.getOptionValues("out")));
+        }
+
+
+
+        //-in <input file> - out <output file>
+    }
+
+}
---
+++ src/main/java/Encrypt.java
@@ -0,0 +1,43 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class Encrypt {
+
+    public static SecretKey generateKey() throws NoSuchAlgorithmException {
+
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        SecureRandom secureRandom = new SecureRandom();
+        int keyBitSize = 256;
+        keyGenerator.init(keyBitSize, secureRandom);
+
+        SecretKey secretKey = keyGenerator.generateKey();
+
+        return secretKey;
+    }
+
+    public static byte[] generateIv() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] iv = new byte[16];
+        secureRandom.nextBytes(iv);
+        return iv;
+    }
+
+    public static void cipherTextStealing(String message, byte[] IV){
+
+        byte[] input = message.getBytes();
+
+        int blocks = message.length()/16;
+
+        byte[] init = IV;
+
+        for(int i =0; i < blocks; i++){
+
+            init[i] = (byte) (input[i]^init[i]);
+        }
+
+        int lastBlock = blocks - 1;
+
+    }
+}
