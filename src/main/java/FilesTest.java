e97a5fdd618473e1501344f0459d8bd6f0683511
Unknown <marionbouteille83@gmail.com>
2019-03-04 14:53:37 +0100

    zip output

--- src/main/java/FilesTest.java
+++ src/main/java/FilesTest.java
@@ -1,7 +1,13 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class FilesTest {
 
@@ -26,5 +32,48 @@ public class FilesTest {
             }
         }
         return false;
+    }
+
+    public static void zip(String zipName, List<String> fileList){
+
+        byte[] buffer = new byte[1024];
+        try {
+            //flux de sortie où écrire les données
+            FileOutputStream fos = new FileOutputStream(zipName);
+            //flux de sortie pour écrire un fichier au format ZIP
+            ZipOutputStream zipOutputStream = new ZipOutputStream(fos);
+            //Méthode de compression pour les entrées non compréssées
+            zipOutputStream.setMethod(ZipOutputStream.STORED);
+
+            for (String file : fileList){
+                System.out.println("Le fichier "+file+"a été ajouté ! ");
+                //Création d'un nouvau fichier zip
+                ZipEntry zipEntry = new ZipEntry(file);
+                zipOutputStream.putNextEntry(zipEntry);
+
+                FileInputStream inputStream = new FileInputStream(file);
+
+                int taille;
+
+                while((taille = inputStream.read(buffer)) > 0){
+                    zipOutputStream.write(buffer,0,taille);
+                }
+
+                inputStream.close();
+
+            }
+
+            zipOutputStream.closeEntry();
+            zipOutputStream.close();
+
+            System.out.println("Fichier compréssé réussi !");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
